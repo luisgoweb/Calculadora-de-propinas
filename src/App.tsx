201d3de24d019d57326e143c57797081ee285ad9
luisgoweb <luismga2012@gmail.com>
2025-06-06 13:08:19 -0400

    App.tsx: Se crea en la main con dos colummnas Menú y Consumo

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,15 @@ function App() {
       <header className="py-10 bg-amber-900">
           <h1 className="text-center text-4xl font-bold text-white">Calculadora de Propinas y Consumo</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto grid md:grid-cols-2 py-10">
+          <div>
+              <h2 className="text-2xl text-center font-bold">Menú</h2>
+          </div>
+          <div>
+              <h2 className="text-2xl text-center font-bold">Consumo</h2>
+          </div>
+      </main>
     </>
   )
 }
