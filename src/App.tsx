f1198386991b923bd27d29ef8092ec9964092b97
luisgoweb <luismga2012@gmail.com>
2025-06-06 12:26:56 -0400

    Base del proyecto con react, typescript, tailwind y vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <header className="py-10 bg-amber-900">
+          <h1 className="text-center text-4xl font-bold text-white">Calculadora de Propinas y Consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
