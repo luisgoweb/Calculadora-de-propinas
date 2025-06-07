b560a615f91b25b96006a15a7f3dbaec0ce0afc1
luisgoweb <luismga2012@gmail.com>
2025-06-07 15:46:41 -0400

    OrderTotals: se calcula el subtotal usando .reduce a order

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,9 @@ function App() {
               deleteItem={deleteItem}
               />
 
-              <OrderTotals />
+              <OrderTotals 
+              order={order}
+              />
               
           </div>
       </main>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,10 +1,20 @@
+import { useMemo } from "react"
+import { formatCurrency } from "../helpers"
+import type { OrderItem } from "../types"
 
+type OrderTotalsProps = {
+    order: OrderItem[]
+}
 
-const OrderTotals = () => {
+const OrderTotals = ({order}: OrderTotalsProps) => {
+    const subTotalAmount = useMemo(()=> order.reduce( (total, item) => total + (item.price * item.quantity), 0),[order]) 
   return (
     <>
         <div className="space-y-3">
             <h3 className="text-2xl  font-bold">Totales y Propina</h3>
+            <p>Subtotal {''} <span className="font-bold">{formatCurrency( subTotalAmount )}</span></p>
+            <p>Propina {''} <span className="font-bold">0</span></p>
+            <p>Total a Pagar {''} <span className="font-bold">0</span></p>
         </div>
         <button className="bg-amber-900 text-white rounded-md cursor-pointer w-full p-3">
             Enviar Orden
