d8b61e35815baca684766dc73b27523ea98395a1
luisgoweb <luismga2012@gmail.com>
2025-06-07 13:57:52 -0400

    useOrder.ts: se agrega state order y setOrder con el nuevo tipado OrderItem que incluye la propiedad quantity

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import type { OrderItem } from "../types";
 
 export const useOrder = ()=>{
-    const[order, setOrder] = useState()
+    const[order, setOrder] = useState<OrderItem[]>([])
 
     return{
 
